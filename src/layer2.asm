01c7ff10a990066b2b3ddb9a1416e7b98234685a
em00k <david.saphier@gmail.com>
2023-07-16 04:50:25 +0100

    Changes to be committed: 	modified:   320Cycle.lst 	modified:   cspect.log 	modified:   src/layer2.asm 	modified:   src/utils.asm

--- src/layer2.asm
+++ src/layer2.asm
@@ -41,136 +41,6 @@ clsL2:
 		ret     
 
 
-draw_map:
-
-	; draws nn tiles from x,y to fill the screen 
-	; IN IX > map to draw 
-	; IN IY > width height 
-
-		ld          a, 0            ; black 
-		call        clsL2           ; clear L2 
-
-		ld 			d, iyh 			; width 
-		ld 			e, iyl			; height 
-		mul 		d, e 	
-		ld 			b, d 
-		ld 			c, e 			; number of tiles to draw  			
-
-		ld          de,$00'00           ; d = x  e = y 
-
-
-.maploop:       ; lets draw 768 tiles 
-	 
-		push        bc              ; save tile counter 
-		ld          h, (ix)         ; get the tile at ix 
-		ld          a, 26           ; bank 
-
-		call        dotile_8x8
-		
-		inc         ix              ; move to next tile 
-		inc         d               ; X + 1 
-		ld          a, d 
-		cp          iyh              ; is X = width?
-
-		jr          nz, .no_inc_e   ; no, then skip to .no_inc_e
-
-		inc         e               ; X=32 so make Y+1
-		ld          d, 0            ; X = 0 
-
-.no_inc_e:
-
-		pop         bc              ; bring back tile counter 
-		dec         bc 
-		ld          a, b 
-		or          c 
-		jr          nz,.maploop 
-
-		ret 
-
-
-dotile_8x8:
-
-	; a > bank 
-	; h > tile 
-	; de > xy 
-
-
-	; Draws a tile h from bank a. de = x y .Total tile size can be 16kb
-	; required bank is auto paged into $0000-$3FFF
-	; 256x192 L2 8x8 256 colour tile
-
-		push        de                                                              ; save XY 
-		bit         7,h			; 8 t 		                                        ; is the tile >=128?
-		jr          z,.noinc	 	; 12 / 7t 	20                                  ; no then jump forward 
-		inc         a 			; 4 		24                                      ; yes increase base bank to 8192-16384
-
-.noinc: 
-
-		nextreg     MMU0_0000_NR_50, a                                              ; set 0 - $1fff ,a 		; set correct bank 
-
-		; Grab tile, x, y 
-		ld          a,h			        ; tile
-		ld          l,d			        ; x
-		ld          h,e			        ; y
-		
-		and         127                 ; we need to wrap over 127 as we have adjusted the base bank 
-
-		; modified from the original dotile8x8 by Michael "Flash" Ware
-		
-		ld          d,64                ; find offset in data, each tile is 8x8 = 64, so multiply 64*tilenumber 
-		ld          e,a					; 11
-		mul         d,e                 ; de holds offset 
-
-		ld          a,%00000000			; tiles at $0
-		or          d		 			; or MSB of offset 
-		ex          de,hl				; swap offset with xy
-		ld          h,a					; 
-		ld          a,e
-		rlca
-		rlca
-		rlca
-		ld          e,a					; 4+4+4+4+4 = 20	; mul x,8
-		ld          a,d
-		rlca
-		rlca
-		rlca
-		ld          d,a					; 4+4+4+4+4 = 20	; mul y,8
-		and         192
-		or          3						; or 3 to keep layer on				; 8
-		ld          bc,LAYER2_ACCESS_P_123B
-		out         (c),a      			; 21			; select bank
-
-		ld          a,d
-		and         63
-		ld          d,a					; clear top bits of y (dest) (4+4+4 = 12)
-		; T96 here
-		ld          a,8					; 7
-.plotTilesLoop2:
-		push        de					; 11
-		ldi
-		ldi
-		ldi
-		ldi
-		ldi
-		ldi
-		ldi
-		ldi		    ; 8 * 16 = 128
-		
-		pop         de					; 11
-		inc         d					; 4 add 256 for next line down
-		dec         a					; 4
-		jr          nz,.plotTilesLoop2			; 12/7
-
-		
-		ld          a,2                 ; writes off, L2 enable 
-		ld          bc,LAYER2_ACCESS_P_123B
-		out         (c),a               ; 21			; select bank
-		pop         de                  ; restore xy 
-
-		ret 
-
-
-
 set_clipping:
 
 	; clips ULA to 0,0
@@ -190,11 +60,9 @@ set_clipping:
 		ret 
 
 
-
 gen_palette:
 
 		; A > offset 
-
 
 		nextreg 		$52,28 								; bring in palette to $4000 
 		ld      		hl, $4000							; point hl to start of palette 
