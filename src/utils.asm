6b7e52db27a40849b0ff7ff8da04e9b159f15048
em00k <david.saphier@gmail.com>
2023-07-16 04:52:10 +0100

    modified:   src/utils.asm

--- src/utils.asm
+++ src/utils.asm
@@ -15,3 +15,22 @@ getRegister:
     pop     bc 
     ret 
 
+
+
+; Vsync wait 
+
+Vsync:	
+            ld      hl, 1
+.readline:	
+			ld 		a,VIDEO_LINE_LSB_NR_1F
+			ld 		bc,TBBLUE_REGISTER_SELECT_P_243B
+			out 	(c),a
+			inc 	b
+			in 		a,(c)
+			cp 		192				; line to wait for 
+			jr 		nz,.readline
+			dec 	hl 
+			ld 		a,h
+			or 		l 
+			jr 		nz,.readline 
+            ret 
